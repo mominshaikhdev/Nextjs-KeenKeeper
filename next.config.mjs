0406ca61d7bfc8197c1efb3e1703cdbbdac1048e
Momin Shaikh <mominshaikhdevs@gmail.com>
2026-04-15 01:12:09 +0600

    adding the initial files

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
